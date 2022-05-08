073c64e2b2cbfc38a6fce7207117e4cf15213ce0
Okellothomas <thomasolal33@gmail.com>
2022-05-08 22:29:12 +0300

    Add sighting methods to the project

--- src/main/java/model/Sighting.java
+++ src/main/java/model/Sighting.java
@@ -1,5 +1,65 @@
 package model;
 
-public class Sighting {
+import java.util.Objects;
 
+public class Sighting {
+    private String name;
+    private String location;
+    private String id;
+    private String animId;
+
+    public Sighting(String name, String location, String id, String animId) {
+        this.name = name;
+        this.location = location;
+        this.id = id;
+        this.animId = animId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAnimId() {
+        return animId;
+    }
+
+    public void setAnimId(String animId) {
+        this.animId = animId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sighting sighting = (Sighting) o;
+        return Objects.equals(name, sighting.name) &&
+                Objects.equals(location, sighting.location) &&
+                Objects.equals(id, sighting.id) &&
+                Objects.equals(animId, sighting.animId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, location, id, animId);
+    }
 }
