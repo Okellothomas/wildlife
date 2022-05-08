949b8d3aee3d09fe227635fc0744162b3d7943e7
Okellothomas <thomasolal33@gmail.com>
2022-05-09 02:50:13 +0300

    Implement methods of sightingDao in Sql2oSighting

--- src/main/java/Db/Sql2oEndangered.java
+++ src/main/java/Db/Sql2oEndangered.java
@@ -1,5 +1,48 @@
 package Db;
 
-public class Sql2oEndangered {
+import model.Endangered;
 
+import java.util.List;
+
+public class Sql2oEndangered implements EndangeredDao {
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public List<Endangered> getAllEndangeredAnimal() {
+        return null;
+    }
+
+    @Override
+    public void addEndangered(Endangered endangered) {
+
+    }
+
+    @Override
+    public Endangered findEndangeredById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteendangeredById(int id) {
+
+    }
+
+    @Override
+    public void updateEngangeredById(int id) {
+
+    }
+
+    @Override
+    public void deleteAllEndangered() {
+
+    }
+
+    @Override
+    public void update(String name, String health, String age) {
+
+    }
 }
--- src/main/java/Db/Sql2oSighting.java
+++ src/main/java/Db/Sql2oSighting.java
@@ -1,5 +1,43 @@
 package Db;
 
-public class Sql2oSighting {
+import model.Sighting;
 
+import java.util.List;
+
+public class Sql2oSighting implements SightingDao {
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public List<Sighting> getAllSighting() {
+        return null;
+    }
+
+    @Override
+    public void addSighting(Sighting sighting) {
+
+    }
+
+    @Override
+    public void update(int id, String location, String name, int animalid) {
+
+    }
+
+    @Override
+    public Sighting findSightingById(int id) {
+        return null;
+    }
+
+    @Override
+    public Sighting deleteById() {
+        return null;
+    }
+
+    @Override
+    public Sighting deleteAllSigting() {
+        return null;
+    }
 }
