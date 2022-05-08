a5b6a2f7ba062dbdb8c90b5e294db0f19437b9d4
Okellothomas <thomasolal33@gmail.com>
2022-05-08 22:24:18 +0300

    Add variables and methods to endangered class

--- src/main/java/model/Animal.java
+++ src/main/java/model/Animal.java
@@ -1,5 +1,55 @@
 package model;
 
+import java.util.Objects;
+
 public class Animal {
 
+    public static final String animal_type = "non-endangered";
+    public String name;
+    public int id;
+    public String animaltype;
+
+    public Animal(String name) {
+        this.name = name;
+        this.animaltype = animal_type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAnimaltype() {
+        return animaltype;
+    }
+
+    public void setAnimaltype(String animaltype) {
+        this.animaltype = animaltype;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return id == animal.id &&
+                Objects.equals(name, animal.name) &&
+                Objects.equals(animaltype, animal.animaltype);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id, animaltype);
+    }
 }
--- src/main/java/model/Endangered.java
+++ src/main/java/model/Endangered.java
@@ -1,5 +1,52 @@
 package model;
 
-public class Endangered {
+import java.util.Objects;
 
+public class Endangered extends Animal{
+    // add variables to class
+    private static final String Animal_Type = "Endangered_Animal";
+    private String health;
+    private String age;
+
+    public Endangered(String name, String health, String age) {
+        super(name);
+        this.health = health;
+        this.age = age;
+        this.animaltype = Animal_Type;
+    }
+
+    public static String getAnimal_Type() {
+        return Animal_Type;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Endangered that = (Endangered) o;
+        return Objects.equals(health, that.health) &&
+                Objects.equals(age, that.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), health, age);
+    }
 }
