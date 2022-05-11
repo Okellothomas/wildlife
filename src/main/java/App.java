19744123edd73338cf51a29bd333cfd7b1bfa8d1
Okellothomas <thomasolal33@gmail.com>
2022-05-11 07:41:41 +0300

    modify the index.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,8 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
@@ -40,5 +39,98 @@ public class App {
             model.put("sighting", allSightings);
             return new ModelAndView(model,"index.hbs");
         },new HandlebarsTemplateEngine());
+
+        //display form to create a new endangered
+        get("/endangered/new",(request,response)->{
+            Map<String, Object>model = new HashMap<>();
+            List<Endangered> endangereds = endangeredDao.getAllEndangeredAnimal();
+            model.put("endangered",endangereds);
+            return new ModelAndView(model,"newendanger.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //process new squad
+        post("/endangered",(request,response)->{
+            Map<String, Object>model = new HashMap<>();
+            String name = request.queryParams("name");
+            String age = request.queryParams("age");
+            String health = request.queryParams("health");
+            String type = request.queryParams("type");
+            Endangered endangered = new Endangered(name, age, health, type);
+            endangeredDao.addEndangered(endangered);
+            response.redirect("endanger.hbs");
+            return null;
+        },new HandlebarsTemplateEngine());
+
+        get("/endangered",(request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+//            List<Animal> allAnimal = animalDao.getAllAnimal();
+//            model.put("animal",allAnimal);
+            List<Endangered> allEndangeredAnimal = endangeredDao.getAllEndangeredAnimal();
+            model.put("endangered",allEndangeredAnimal);
+//            List<Sighting> allSightings = sightingDao.getAllSighting();
+//            model.put("sighting", allSightings);
+            return new ModelAndView(model,"endanger.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //clear all endangered
+        get("/endengered/delete",(request, response)->{
+            Map<String,Object>model = new HashMap<>();
+            endangeredDao.deleteAllEndangered();
+            response.redirect("/");
+            return null;
+        },new HandlebarsTemplateEngine());
+
+        //clear all animals
+        get("/animal/delete",(request, response)->{
+            Map<String,Object>model = new HashMap<>();
+            animalDao.deleteAllAnimal();
+            response.redirect("/");
+            return null;
+        },new HandlebarsTemplateEngine());
+
+        //clear all sighting
+        get("/sightings/delete",(request, response)->{
+            Map<String,Object>model = new HashMap<>();
+            sightingDao.deleteAllSigting();
+            response.redirect("/");
+            return null;
+        },new HandlebarsTemplateEngine());
+
+
+        get("/endangered/:id",(request,response)->{
+            Map<String,Object>model = new HashMap<>();
+            int endangeredToFindId = Integer.parseInt(request.params("id"));
+            Endangered endangered = endangeredDao.findEndangeredById(endangeredToFindId);
+            model.put("endangered",endangeredDao.getAllEndangeredAnimal());
+            return new ModelAndView(model,"endanger.hbs");
+        }, new HandlebarsTemplateEngine());
+
+//        get("/",(request, response) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            List<Animal> allAnimal = animalDao.getAllAnimal();
+//            model.put("animal",allAnimal);
+//            List<Endangered> allEndangeredAnimal = endangeredDao.getAllEndangeredAnimal();
+//            model.put("endangered",allEndangeredAnimal);
+//            List<Sighting> allSightings = sightingDao.getAllSighting();
+//            model.put("sighting", allSightings);
+//            return new ModelAndView(model,"index.hbs");
+//        },new HandlebarsTemplateEngine());
+
+        //display form to create a new animal
+        get("/animal/new",(request,response)->{
+            Map<String, Object> model = new HashMap<>();
+            List<Animal> animals = animalDao.getAllAnimal();
+            model.put("animal",animals);
+            return new ModelAndView(model,"newanimal.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //display form to create a new animal
+        get("/sighting/new",(request,response)->{
+            Map<String, Object> model = new HashMap<>();
+            List<Sighting> sightings = sightingDao.getAllSighting();
+            model.put("sighting",sightings);
+            return new ModelAndView(model,"newsighting.hbs");
+        },new HandlebarsTemplateEngine());
     }
+
 }
